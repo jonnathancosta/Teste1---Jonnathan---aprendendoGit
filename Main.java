c7422720e15fadb0b00ddbde0f5bd9cde099f79a
Jonnathan Costa <jonnathan.costa@outlook.com>
2023-01-02 14:15:05 -0300

    Testando git commit inicial

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.println("Digite a quantidade de termos");
+        int n1 = 0, n2 = 1;
+        for (int qtd = x.nextInt(); qtd > 0; qtd--) {
+            System.out.print(n1 + " ");
+            int n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+        System.out.println("Fim");
+    }
+}
---
+++ Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa{
+ public String nome;
+ public double idade;
+public Pessoa(){
+
+}
+public Pessoa(String n, double i){
+    this.nome =n;
+    this.idade = i;
+}
+}
